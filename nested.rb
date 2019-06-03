4a380a8c8a9986fc5505e156cf20ca62a5d17b39
Alyssa Yesilyurt <alyssa.yesilyurt@gmail.com>
2019-06-03 02:45:38 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,13 +15,10 @@ def hopper
         }
      }
     programmer_hash[:grace_hopper]
-    programmer_hash[:alan_kay][:known_for]
 
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -37,6 +34,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+   programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
